908ba66868f6e12f8d6a81cd1ec3af0b0721b040
DAMMOU <udammou@gmail.com>
2025-09-30 04:34:00 +0200

    feat: add contact page and functionality

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,5 +5,6 @@ import { Login } from './components/login/login';
 export const routes: Routes = [
     { path: '', component: Home },
     { path: 'login', component: Login },
+    {path: 'contact', loadComponent: () => import('./components/contact/contact').then(m => m.Contact) },   
     { path: '**', redirectTo: '' }  // Redirection pour les routes inexistantes
 ];
