f84b5f5dec728da9a5222862697c95f040578537
DAMMOU <udammou@gmail.com>
2025-10-08 18:56:21 +0200

    Merge remote changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,10 +1,6 @@
 export const environment = {
   production: true,
-<<<<<<< HEAD
   apiUrl: 'https://r2e-backend.onrender.com/api',
-=======
-  apiUrl: 'https://ton-app-render.onrender.com/api',
->>>>>>> 8521aa4d9fb354604f21a28230b29c88bd85b603
   appName: 'Ready2Entreprise',
   version: '1.0.0'
 };
